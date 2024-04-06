b1fe0d41af5df57c9bf75b54b8491fb8dd6b5c0a
blue32a <blue32a.jp@gmail.com>
2024-04-06 10:08:26 +0000

    feat: リクエストパラメーターを使用する関数に更新

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const functions = require('@google-cloud/functions-framework');
 
-functions.http('hello', (_, res) => {
-  res.send('Hello, world!');
+functions.http('hello', (req, res) => {
+  const name = req.query.name || 'World';
+  res.send(`Hello, ${name}!`);
 });
